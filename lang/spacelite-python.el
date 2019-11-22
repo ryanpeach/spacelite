09778aa7d6dba86635ca13c2da74a478b6e1a5bf
Ryan Peach <ryan.peach@keysight.com>
2019-11-22 18:05:48 -0500

    Fixed python, added syntax highlighting

--- lang/spacelite-python.el
+++ lang/spacelite-python.el
@@ -2,24 +2,38 @@
   ;; TODO: Required for setting up your python environment
   (setq python-shell-interpreter "ipython3")
   (setq python-shell-interpreter-args "-i --simple-prompt")
+  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 
   ;; Flycheck
   (use-package
     flycheck
     :defer t
+    :init
+
+    ;; TODO: Why doesn't the prefix work?
+    (spacelite/declare-prefix-for-mode 'python-mode "s" "syntax" "flycheck-syntax")
+    (spacelite/set-leader-keys-for-major-mode 'python-mode "ss" 'flycheck-mode)
+    (spacelite/set-leader-keys-for-major-mode 'python-mode "sn" 'flycheck-next-error)
+    (spacelite/set-leader-keys-for-major-mode 'python-mode "sp" 'flycheck-previous-error)
+
+    ;; Only flycheck on save, but always keep enabled
+    (global-flycheck-mode)
+
     :config
+
     (flycheck-define-checker
-     python-mypy ""
-     :command ("mypy"
-	       "--ignore-missing-imports" "--fast-parser"
-	       "--python-version" "3.6"
-	       source-original)
-     :error-patterns
-     ((error line-start (file-name) ":" line ": error:" (message) line-end))
-     :modes python-mode)
+        python-mypy ""
+        :command ("mypy"
+                  "--ignore-missing-imports" "--fast-parser"
+                  "--python-version" "3.6"
+                  source-original)
+        :error-patterns
+        ((error line-start (file-name) ":" line ": error:" (message) line-end))
+        :modes python-mode)
 
     (add-to-list 'flycheck-checkers 'python-mypy t)
     (flycheck-add-next-checker 'python-pylint 'python-mypy t)
+    (setq flycheck-check-syntax-automatically '(mode-enabled save))
     )
 
   ;; Used for many python related tasks
@@ -54,7 +68,22 @@
     (defalias 'elpy-goto-return 'pop-tag-mark)
     (spacelite/set-leader-keys-for-major-mode 'python-mode "." 'elpy-goto-definition)
     (spacelite/set-leader-keys-for-major-mode 'python-mode ">" 'elpy-goto-definition-other-window)
-    (spacelite/set-leader-keys-for-major-mode 'python-mode "*" 'elpy-goto-return))
+    (spacelite/set-leader-keys-for-major-mode 'python-mode "*" 'elpy-goto-return)
+
+
+    :config
+    ;; Get rid of flymake in favor of flycheck
+    (setq elpy-modules '(elpy-module-sane-defaults
+                         elpy-module-company
+                         elpy-module-eldoc
+                         ;; elpy-module-flymake
+                         elpy-module-highlight-indentation
+                         elpy-module-pyvenv
+                         elpy-module-yasnippet
+                         ;; elpy-module-django
+                         )
+          )
+    )
 
   ;; (use-package
   ;;   ropemacs
