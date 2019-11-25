3c9136905c1804401fbd2f6a6aef18f7c5f77326
Ryan Peach <ryan.peach@keysight.com>
2019-11-24 23:12:00 -0500

    Automatic

--- utils/spacelite-yasnippet.el
+++ utils/spacelite-yasnippet.el
@@ -6,6 +6,13 @@
       :defer t)
     (yas-global-mode 1)
     (add-hook 'after-init-hook 'global-company-mode)
+
+    ;; Keybindings
+    (spacelite/declare-prefix "y" "yasnippet")
+    (spacelite/set-leader-keys "yi" 'yas-insert-snippet)
+    (spacelite/set-leader-keys "yc" 'yas-new-snippet)
+
+    ;; Your directory
     (setq yas-snippet-dirs
           '("~/.emacs.d/snippets") ;; personal snippets
           )
