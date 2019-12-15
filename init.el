6de4d320d9a596222db8d9d1a2f1b507dd4a6374
Ryan Peach <ryan.peach@keysight.com>
2019-12-15 18:24:44 -0500

    Added the ability to automatically install use-packages

--- init.el
+++ init.el
@@ -4,6 +4,20 @@
 (or (server-running-p)
     (server-start))
 
+;; Auto install use-package
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
 (use-package benchmark-init
   :ensure t
   :config
